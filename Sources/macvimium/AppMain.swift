85683f969b379765bcfd0738aba9444dc5d64a33
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2026-04-02 19:34:14 -0700

    Retain app delegate for process lifetime

--- Sources/macvimium/AppMain.swift
+++ Sources/macvimium/AppMain.swift
@@ -1,10 +1,12 @@
 import AppKit
 
+@MainActor
 @main
 struct AppMain {
+    private static let delegate = AppDelegate()
+
     static func main() {
         let application = NSApplication.shared
-        let delegate = AppDelegate()
         application.setActivationPolicy(.accessory)
         application.delegate = delegate
         application.run()
